a86dbab6978d51d7398cebf5b29ce2064f64c8de
NataliekohNP <s10227870@connect.np.edu.sg>
2022-02-08 12:51:57 +0800

    pages uploaded

---
+++ javascript/signup.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://userdata-6076.restdb.io/rest/userlogin",
+        "method": "GET",
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": "61fb6b74fd4d376be83fe021",
+          "cache-control": "no-cache"
+        }
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+    
+    
+      });
+      
+      $("#sign-up-submit").on("click", function(e){
+        e.preventDefault();
+        adduser();
+      })
+
+      let username = $("#name").val();
+      let email = $("#email").val();
+      let password = $("#password").val();
+
+      let jsondata={
+          "Username" : username,
+          "Email": email,
+          "Password":password
+      }
+      function adduser(all = true) {
+        let username = $("#name").val();
+        let email = $("#email").val();
+        let password = $("#password").val();
+  
+        let jsondata={
+            "Username" : username,
+            "Email": email,
+            "Password":password
+        };
+        //[STEP 7]: Create our AJAX settings
+        let settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://userdata-6076.restdb.io/rest/userlogin",
+          "method": "POST", //[cher] we will use GET to retrieve info
+          "headers": {
+            "content-type": "application/json",
+            "x-apikey": "61fb6b74fd4d376be83fe021",
+            "cache-control": "no-cache"
+          },
+          "processData": false,
+          "data": JSON.stringify(jsondata)
+          
+        }
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            window.location.href = "../html/login.html"
+            
+          });
+      
+    
+    
+      }
+    })
